d33dd413f11cadd7843a2420652221d565d62176
IbrahimAhmedAly <ibrahim.zagglol@gmail.com>
2023-06-26 18:35:50 +0200

    allow origin

--- src/index.js
+++ src/index.js
@@ -28,6 +28,12 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(express.json());
+// middlewares
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(
   cors({
     origin: "http://localhost:3000",
