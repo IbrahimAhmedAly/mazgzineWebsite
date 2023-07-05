fc015d4a39ba812bec53d56ac137524ab80ee4ab
IbrahimAhmedAly <ibrahim.zagglol@gmail.com>
2023-07-05 10:16:09 +0200

    index finished

--- src/index.js
+++ src/index.js
@@ -74,6 +74,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(express.json());
+app.use(express.static("uploads"));
 // middlewares
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
